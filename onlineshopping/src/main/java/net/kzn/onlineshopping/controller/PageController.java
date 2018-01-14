dd751839e03f5a95eedec61c14036e74263364a2
Dilendra Pant <pant.dilendra@gmail.com>
2018-01-15 02:42:43 +0530

    interface theme etc.

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -12,30 +12,23 @@ public class PageController {
 	@RequestMapping(value={"/","/home","/index"})
 	public ModelAndView index(){
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "Welcome to new spring mvc page !!!!!!!");
+		mv.addObject("tiltle", "Home");
+		mv.addObject("userClickHome", true);
 		return mv;
 	}
-	
-	@RequestMapping(value="/test")
-	public ModelAndView test(@RequestParam (value="greeting",required=false) String gt){
-		
-		if(gt==null){
-			gt="Hello There !!!!";
-		}
+	@RequestMapping(value={"/about"})
+	public ModelAndView about(){
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", gt);
+		mv.addObject("tiltle", "About Us");
+		mv.addObject("userClickAbout", true);
 		return mv;
 	}
-	
-	
-	@RequestMapping(value="/test1/{greeting}")
-	public ModelAndView test1(@PathVariable (value="greeting",required=false) String gt){
-		
-		if(gt==null){
-			gt="Hello There !!!!";
-		}
+	@RequestMapping(value={"/contact"})
+	public ModelAndView contact(){
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", gt);
+		mv.addObject("tiltle", "Contact Us");
+		mv.addObject("userClickContact", true);
 		return mv;
 	}
+	
 }
