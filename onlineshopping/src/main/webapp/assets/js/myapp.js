7729edb255f3cbf2a13ad5ab3adac25364aec731
Dilendra Pant <pant.dilendra@gmail.com>
2018-02-22 01:43:39 +0530

    jQuery for updating dataTable inside myapp.js

--- onlineshopping/src/main/webapp/assets/js/myapp.js
+++ onlineshopping/src/main/webapp/assets/js/myapp.js
@@ -1,23 +1,46 @@
 $(function() {
 	// solving the active menu problem
-	switch(menu) {
-	
-		case 'About Us':
-			$('#about').addClass('active');
+	switch (menu) {
+
+	case 'About Us':
+		$('#about').addClass('active');
+		break;
+	case 'Contact Us':
+		$('#contact').addClass('active');
+		break;
+	case 'All Products':
+		$('#listProducts').addClass('active');
+		break;
+	default:
+		if (menu == "Home")
 			break;
-		case 'Contact Us':
-			$('#contact').addClass('active');
-			break;
-		case 'All Products':
-			$('#listProducts').addClass('active');
-			break;
-		default:
-			if(menu=="Home") break;
-			$('#listProducts').addClass('active');
-			$('#a_'+menu).addClass('active');
-			break;	
-	
+		$('#listProducts').addClass('active');
+		$('#a_' + menu).addClass('active');
+		break;
+
 	}
-		
+
+	// JQuery DataTable
+
+	var products = [ [ '1', 'ABC' ], [ '2', 'DEF' ], [ '3', 'GHI' ],
+			[ '4', 'JKL' ], [ '5', 'MNO' ], [ '6', 'PQR' ], [ '7', 'STU' ],
+			[ '8', 'VWX' ], ];
 	
+	var $table=$('#productListTable');
+	
+	// Execute only when table is available.
+	
+	if($table.length){
+		//console.log('Inside the table!');
+		$table.DataTable({
+			lengthMenu:[[3,5,10,-1],['3 Records', '5 Records','10 Records','All']],
+			pageLength:5,
+			data:products
+		});
+		
+	}
+	
+	
+	
+
 });
